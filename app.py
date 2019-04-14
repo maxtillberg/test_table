991c77301b1db110c697f89f72f981f6cf7dbe3f
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-04-14 18:12:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,16 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
+import dash_table
+import pandas as pd
 
-########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
-bitterness = go.Bar(
-    x=beers,
-    y=[35, 60, 85, 75],
-    name='IBU',
-    marker={'color':'red'}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=[5.4, 7.1, 9.2, 4.3],
-    name='ABV',
-    marker={'color':'blue'}
-)
+app = dash.Dash(__name__)
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = 'Beer Comparison'
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-########### Display the chart
-
-app = dash.Dash()
-server = app.server
-
-app.layout = html.Div(children=[
-    html.H1('Flying Dog Beers'),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    )]
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict("rows"),
 )
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
