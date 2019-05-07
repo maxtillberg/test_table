e3fabacedbc7b6c2f7be9fbbf62bab846a0f43df
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-05-07 09:59:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import pandas as pd
 
 #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 df = pd.read_csv('2011_us_ag_exports.csv')
+df1 = df[0:6]
 app = dash.Dash(__name__)
 
 server = app.server
 
 app.layout = dash_table.DataTable(
     id='table',
-    columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict("rows"),
+    columns=[{"name": i, "id": i} for i in df1.columns],
+    data=df1.to_dict("rows"),
 )
 
 if __name__ == '__main__':
